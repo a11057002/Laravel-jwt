809ba92f9b3670abfece3187d42a9bf3121a2ebf
andy870622 <35159550+andy870622@users.noreply.github.com>
2021-07-30 20:06:10 +0800

    typo

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -9,7 +9,7 @@ class AuthController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['login', 'payload']]);
+        $this->middleware('auth:api', ['except' => ['login','payload']]);
     }
 
     // 登入並回傳 jwt Token
@@ -20,17 +20,17 @@ class AuthController extends Controller
         // TODO: 了解 guard 怎麼切換
         // 因為 guard 換成 jwt, attempt function 的位置在 Tymon\JWTAuth\JWTGuard;
         // 嘗試用 request 中的參數認證使用者
-        if (!$token = auth()->claims(['test' => '123444'])->attempt($credentials)) {
+        if (!$token = auth()->attempt($credentials)) {
             return response()->json(['error' => 'Unauthorized'], 401);
         }
         return $this->respondWithToken($token);
     }
 
     // 驗證 jwt token 回傳持有人
-    public function me()
+    public function verify()
     {
         // auth() 是 authManager， 在Illuminate\Auth\AuthManager
-        return response()->json(auth()->user());
+        return response()->json(['name'=>auth()->user()->name]);
     }
 
     // 銷毀傳進的 jwtToken
@@ -60,14 +60,22 @@ class AuthController extends Controller
 
     public function payload()
     {
+        // iss   jwt 的發行方
+        // iat   jwt 發行時間 issued at time
+        // exp   過期時間 expiration time
+        // nbf   (跟iat一樣?) 當前時間不能早於這個時間 not before time
+        // jti   jwt id 確保唯一性
+        // sub   識別訊息 subject ??
+        // prv   User Provider 的 hash 結果  https://github.com/tymondesigns/jwt-auth/issues/1344
+
         $payload = auth()->payload();
         return response()->json($payload);
     }
 
-    // 提供帳密 和 jwt 做驗證
-    public function jwtValidate()
-    {
-        $credentials = request(['email', 'password']);
-        return auth()->validate($credentials)==true ? auth()->validate($credentials) : response()->json(['message' => 'failed']);
-    }
+    // 不太確定 validate 用法
+    // public function jwtValidate()
+    // {
+    //     $credentials = request(['email', 'password']);
+    //     return auth()->validate($credentials)==true ? auth()->validate($credentials) : response()->json(['message' => 'failed']);
+    // }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,7 +22,7 @@ Route::group(['middleware' => 'api','prefix' => 'auth'], function ($router) {
     Route::post('login', 'AuthController@login');
     Route::post('logout', 'AuthController@logout');
     Route::post('refresh', 'AuthController@refresh');
-    Route::post('me', 'AuthController@me');
+    Route::post('verify', 'AuthController@verify');
     Route::post('payload','AuthController@payload');
     Route::post('jwtValidate','AuthController@jwtValidate');
 });
